c6ba82fd9666bff7f81077d38fd1c19b09d31213
ErnaniUlsenheimer <ernaniu@gmail.com>
2023-05-05 16:22:39 -0300

    Deploy execute Query

--- Deploy/cidade.sql
+++ Deploy/cidade.sql
@@ -1,1 +1,1 @@
-create table cidade(id number not null, nome varchar2(40) not null, estado varchar2(200) null)
+create table cidade (id number not null, nome varchar2(40) not null, estado varchar2(200) null)
