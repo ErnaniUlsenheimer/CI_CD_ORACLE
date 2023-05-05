a4a52ead43813c1405ef150e6735f5897bccca36
ErnaniUlsenheimer <ernaniu@gmail.com>
2023-05-05 16:57:53 -0300

    Verifica Deploy sql

--- Verify/cidade.sql
+++ Verify/cidade.sql
@@ -1,1 +1,1 @@
-SELECT object_name as table_name FROM user_objects WHERE object_type = 'TABLE' AND object_name = 'USUARIO' ORDER BY object_name
+SELECT object_name as table_name FROM user_objects WHERE object_type = 'TABLE' AND object_name = 'CIDADE' ORDER BY object_name
