7279ca970d7ca9aab6031a5db378bc5202ce5b14
ErnaniUlsenheimer <ernaniu@gmail.com>
2023-05-08 09:15:03 -0300

    Revert CD Jenkins

--- Revert/cidade.sql
+++ Revert/cidade.sql
@@ -1,8 +1,1 @@
-declare
-   c int;
-begin
-   select count(*) into c from user_tables where table_name = upper('cidade');
-   if c = 1 then
-      execute immediate 'drop table cidade';
-   end if;
-end;
+DROP table cidade
